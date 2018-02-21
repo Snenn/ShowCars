da88715d6f8ca3a857b648116248f0947fde22fe
Nikolay Snegurskiy <snenn9@gmail.com>
2018-02-21 00:24:11 -0500

    fix servlet

--- web/src/main/java/com/showCars/ServletShowCars.java
+++ web/src/main/java/com/showCars/ServletShowCars.java
@@ -2,17 +2,22 @@ package com.showCars;
 
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+@WebServlet("/")
 public class ServletShowCars extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+        response.setContentType("text/html");
         //List<Car> cars = Dao.getDAO().getAll();
         //response.getWriter().println(cars);
         PrintWriter out = response.getWriter();
