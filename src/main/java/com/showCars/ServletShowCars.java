c54efb92e5b95e29e1b467cdd454dd87e3b8d3cc
Nikolay Snegurskiy <snenn9@gmail.com>
2018-03-27 14:15:13 -0400

    fix servlet

--- src/main/java/com/showCars/ServletShowCars.java
+++ src/main/java/com/showCars/ServletShowCars.java
@@ -1,9 +1,6 @@
 package com.showCars;
 
 
-import com.showCars.dao.Dao;
-import com.showCars.pojos.Car;
-
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +8,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 
 @WebServlet(
         name = "MyServlet",
@@ -23,9 +19,7 @@ public class ServletShowCars extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         ServletOutputStream out = resp.getOutputStream();
-        List<Car> cars = Dao.getDAO().getAll();
         out.write("hello heroku".getBytes());
-        out.println(cars.toString());
         out.flush();
         out.close();
     }
